2a30e19ef59751e4f063b7b64d3da6713078c091
bingsa-tech <ton_email@exemple.com>
2025-08-29 05:40:28 -0400

    1 commit frontend

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // ton backend
+  baseURL: "https://health-now-gitspace.onrender.com", // ton backend
   withCredentials: true,
 });
 
