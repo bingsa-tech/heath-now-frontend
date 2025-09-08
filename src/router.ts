f69f0ca9d840f98900eababec57f6f3916676f7a
bingsa-tech <biljobs@outlook.com>
2025-09-08 13:28:31 -0400

    add chatbox route

--- src/router.ts
+++ src/router.ts
@@ -2,13 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "./pages/Login.vue";
 import Register from "./pages/Register.vue";
 import Dashboard from "./pages/Dashboard.vue";
+import Chatbot from "./pages/Chatbot.vue";
 
 const routes = [
   { path: "/", redirect: "/dashboard" },
   { path: "/login", component: Login },
   { path: "/register", component: Register },
   { path: "/dashboard", component: Dashboard },
-  {path : "/chat", component: () => import("./pages/Chatbot.vue") }
+  {path : "/chat", component : Chatbot}
 ];
 
 const router = createRouter({
